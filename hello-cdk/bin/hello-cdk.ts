93359f479226e33f326ebd842f057936c4554e88
Paul Tobin <tobster111@gmail.com>
2024-10-30 21:13:57 +0000

    Add environment configuration for HelloCdkStack with region and account

--- hello-cdk/bin/hello-cdk.ts
+++ hello-cdk/bin/hello-cdk.ts
@@ -5,6 +5,10 @@ import { HelloCdkStack } from '../lib/hello-cdk-stack';
 
 const app = new cdk.App();
 new HelloCdkStack(app, 'HelloCdkStack', {
+  env: {
+    region: 'eu-west-1',
+    account: 
+  }
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
