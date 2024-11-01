5c4650e84f40898614e9226f802331837d4da38c
Paul Tobin <tobster111@gmail.com>
2024-11-01 09:51:19 +0000

    Add dotenv support and update environment configuration in HelloCdkStack

--- hello-cdk/bin/hello-cdk.ts
+++ hello-cdk/bin/hello-cdk.ts
@@ -5,7 +5,10 @@ import { HelloCdkStack } from "../lib/hello-cdk-stack";
 
 const app = new cdk.App();
 new HelloCdkStack(app, "HelloCdkStack", {
-  env: { region: "eu-west-1", account: "123456789012" },
+  env: {
+    account: process.env.AWS_ACCOUNT_ID,
+    region: process.env.AWS_REGION,
+  },
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
